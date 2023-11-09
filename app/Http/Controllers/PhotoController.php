ea94080ccb2cc5229ffdda0e7d47e73ad905e0b0
Wilmer Suclupe Farroñan <124706662+wilmersf2001@users.noreply.github.com>
2023-11-09 18:48:38 -0500

    modificar el storage

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PhotoController extends Controller
 {
     public function store(Request $request)
     {
-        dd($request->all());
+        if ($request->hasFile('frontDniPhoto')) {
+            $image = $request->file('frontDniPhoto');
+            $fileName = time() . '.' . $image->getClientOriginalExtension();
+
+            Storage::disk('public')->put('fotos-validas/' . $fileName, file_get_contents($image));
+        }
+
+        $url = Storage::url('fotos-validas/' . $fileName);
+        dd($url);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,85 +11,24 @@
 
 <body>
 
-    {{-- <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8 justify-items-center">
+    <form action="{{ route('photo.store') }}" method="POST" enctype="multipart/form-data">
+        @csrf
         <div class="group relative">
-            <div
-                class="relative aspect-h-1 aspect-w-1 w-60 overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-80">
-                <div class="h-full w-full">
-                    <img src="{{ asset('recursos/foto-carnet.jpg') }}" alt=""
-                        class="h-full w-full object-cover object-center lg:h-full lg:w-full">
-                </div>
-                <div class="absolute inset-0 flex items-center justify-center">
-                    <div class="text-red-500 text-9xl">X</div>
-                </div>
-            </div>
-
-            <label class="block mt-4">
-                <span
-                    class="after:content-['*'] after:ml-0.5 after:text-red-500 block mb-2 text-sm font-medium text-gray-900">
-                    Foto de Carnet
-                </span>
-                <input type="file" wire:model="profilePhoto" wire:click="$set('profilePhoto', null)"
-                    class="block w-full text-sm text-slate-500
-                              file:mr-4 file:py-2 file:px-4
-                              file:rounded-full file:border-0
-                              file:text-ms file:font-semibold
-                              file:bg-blue-50 file:text-blue-700
-                              hover:file:bg-blue-100
-                            " />
-                <x-input-error for="profilePhoto" />
-            </label>
-        </div>
-
-        <div class="group relative">
-            <div
-                class="aspect-h-1 aspect-w-1 w-60 overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-80 flex items-center">
-                <div>
-                    <img src="{{ asset('recursos/dni-anverso.jpg') }}" alt="">
-                </div>
-            </div>
             <label class="block mt-4">
                 <span
                     class="after:content-['*'] after:ml-0.5 after:text-red-500 block mb-2 text-sm font-medium text-gray-900">
                     Foto DNI Anverso
                 </span>
-                <input type="file" wire:model="frontDniPhoto" wire:click="$set('frontDniPhoto', null)"
-                    class="block w-full text-sm text-slate-500
-                              file:mr-4 file:py-2 file:px-4
-                              file:rounded-full file:border-0
-                              file:text-ms file:font-semibold
-                              file:bg-blue-50 file:text-blue-700
-                              hover:file:bg-blue-100
-                            " />
-                <x-input-error for="frontDniPhoto" />
+                <input type="file" name="frontDniPhoto"
+                    class="block w-full
+                    text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-ms
+                    file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100 " />
             </label>
         </div>
-
-        <div class="group relative">
-            <div
-                class="aspect-h-1 aspect-w-1 w-60 overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-80 flex items-center">
-                <div>
-                    <img src="{{ asset('recursos/dni-reverso.jpg') }}" alt="Front of men&#039;s Basic Tee in black.">
-                </div>
-            </div>
-            <label class="block mt-4">
-                <span
-                    class="after:content-['*'] after:ml-0.5 after:text-red-500 block mb-2 text-sm font-medium text-gray-900">
-                    Foto DNI Reverso
-                </span>
-                <input type="file" wire:model="reverseDniPhoto" wire:click="$set('reverseDniPhoto', null)"
-                    class="block w-full text-sm text-slate-500
-                              file:mr-4 file:py-2 file:px-4
-                              file:rounded-full file:border-0
-                              file:text-ms file:font-semibold
-                              file:bg-blue-50 file:text-blue-700
-                              hover:file:bg-blue-100
-                            " />
-                <x-input-error for="reverseDniPhoto" />
-            </label>
-        </div>
-
-    </div> --}}
+        <br>
+        <br>
+        <button type="submit">click</button>
+    </form>
 </body>
 
 </html>
