90143c2d3084a5fbeb89155609397fde1243552e
Wilmer Suclupe Farroñan <124706662+wilmersf2001@users.noreply.github.com>
2024-01-05 13:55:23 -0500

    Update label for document number field

--- resources/views/livewire/pay.blade.php
+++ resources/views/livewire/pay.blade.php
@@ -58,7 +58,7 @@
                 <label class="block mb-6">
                     <span
                         class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-900">
-                        Número de documento
+                        Número de documento de identidad (DNI o CE)
                     </span>
                     <input type="text" name="numDocument" wire:model="numDocument" minlength="8" maxlength="9"
                         class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
